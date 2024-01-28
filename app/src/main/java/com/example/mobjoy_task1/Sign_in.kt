bb2365bafc6a630b23aa07ed413f4da81724d48e
Khaled Goda <khaledgoda179@gmail.com>
2024-01-28 18:50:05 +0200

    initial commit

---
+++ app/src/main/java/com/example/mobjoy_task1/Sign_in.kt
@@ -0,0 +1,11 @@
+package com.example.mobjoy_task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_in : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
